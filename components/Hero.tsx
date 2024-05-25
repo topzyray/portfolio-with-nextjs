cf86973c4b626762fbaedd19755f43dcff4a340d
Tope Taiwo <topzyray@yahoo.com>
2024-05-25 21:12:22 +0100

    First commit

---
+++ components/Hero.tsx
@@ -0,0 +1,70 @@
+import { FaLocationArrow } from "react-icons/fa";
+import MagicButton from "./ui/MagicButton";
+import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import Link from "next/link";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="top-10 left-full h-[80vh] w-[50vw]"
+          fill="purple"
+        />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      </div>
+
+      <div className="flex justify-center relative my-20 z-10">
+        {/* Implement a toggle theme */}
+        {/* <button className="text-white fixed top-5 right-5">Toggle Theme</button> */}
+
+        {/* My Avatar */}
+        <Link
+          href="https://github.com/topzyray"
+          target="
+        _blank"
+        >
+          <img
+            src="/tope.png"
+            alt="Tope Taiwo"
+            className="z-50 w-8 md:w-10 lg:w-12 text-white fixed top-2 left-2 md:top-5 md:left-5 rounded-full shadow-2xl hover:border-white-200"
+          />
+        </Link>
+
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Dynamic Web Magic with Next.js
+          </h2>
+
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Transforming Concepts into Seamless User Experiences"
+          />
+
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi, I&apos;m Tope Taiwo, a Software Developer based in Nigeria.
+          </p>
+
+          <a href="#about">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ data/index.ts
@@ -0,0 +1,235 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a Full-Stack Property Listing Platform",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Onile Property with Next.js",
+    des: "Onile is a premier property listing platform that seamlessly connects property seekers with landlords and owners for both rentals and sales. Whether you're looking to rent or buy, Onile provides a streamlined, user-friendly experience to help you find your perfect property.",
+    img: "/onile2.png",
+    iconLists: ["/next.svg", "/tail.svg", "/js.svg", "/fm.svg"],
+    link: "https://onile-redesign.vercel.app",
+  },
+  {
+    id: 2,
+    title: "My Portfolio",
+    des: "Welcome to my portfolio! Here, you'll find a showcase of my latest projects, highlighting my skills and creativity. From innovative designs to intricate code, each piece reflects my passion and dedication to excellence. Explore my work and see how I can bring value to your next project.",
+    img: "/portfolio.png",
+    iconLists: [
+      "/next.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "acernity.webp",
+      "/fm.svg",
+    ],
+    link: "https://github.com/topzyray",
+  },
+  {
+    id: 3,
+    title: "Onile Property - Phase 1",
+    des: "Onile is a premier property listing platform that seamlessly connects property seekers with landlords and owners for both rentals and sales. Whether you're looking to rent or buy, Onile provides a streamlined, user-friendly experience to help you find your perfect property.",
+    img: "/onile1.png",
+    iconLists: ["/react.svg", "/bootstrap.svg", "/js.svg"],
+    link: "https://onile-homes.netlify.app",
+  },
+  {
+    id: 4,
+    title: "Unleashing the Power of Creativity: GPT-3",
+    des: "I turned the lovely GPT-3 Figma design to code. GPT-3 design by Andrian.",
+    img: "/gpt3.png",
+    iconLists: ["/react.svg", "/css.svg", "/js.svg"],
+    link: "https://github.com/topzyray/gpt3.git",
+  },
+  {
+    id: 5,
+    title: "Blogging API in Node.js with Express",
+    des: "A Blogging API created with Node.js with Express framework, database in MongoDB and hosed on Render.com.",
+    img: "/nodejs.svg",
+    iconLists: ["/nodejs.svg", "/mongodb.svg", "/js.svg"],
+    link: "https://github.com/topzyray/blogging-api.git",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Freelancer Developer",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Community Ambassador at VTF",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 3,
+    title: "Upskilling and Reskilling",
+    desc: "Acquiring skills further skills in Full-stack development.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+ 
+  // {
+  //   id: 4,
+  //   title: "Lead Frontend Developer",
+  //   desc: "Developed and maintained user-facing features using modern frontend technologies.",
+  //   className: "md:col-span-2",
+  //   thumbnail: "/exp4.svg",
+  // },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/topzyray",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://twitter.com/taiwotopesunday",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://linkedin.com/in/taiwotopesunday",
+  },
+];
